64fcb07e679cf8b4b93af5001a4973c957d49ec4
Aviram Boniel <aviramb123@gmail.com>
2020-06-28 15:41:11 +0300

    Updates

---
+++ precache-manifest.8c660064b6e2b82aeeb3d7637333afb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c37a18298b8267196aaad689b8c05283",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "86947ba3186e66110d15",
+    "url": "/contactManager/static/css/2.f93178fb.chunk.css"
+  },
+  {
+    "revision": "069a046ec1186510b88a",
+    "url": "/contactManager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "86947ba3186e66110d15",
+    "url": "/contactManager/static/js/2.2bb3344f.chunk.js"
+  },
+  {
+    "revision": "f88b36470b8b650da4a18244e35dd001",
+    "url": "/contactManager/static/js/2.2bb3344f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "069a046ec1186510b88a",
+    "url": "/contactManager/static/js/main.c5fcb225.chunk.js"
+  },
+  {
+    "revision": "16f36bd88c123f457ada",
+    "url": "/contactManager/static/js/runtime-main.c7a2fccb.js"
+  }
+]);
